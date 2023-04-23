57153aa1eb3232b19daff0d842d0b5e9b73190fa
Denis <den.dvornikov.88@inbox.ru>
2023-04-23 22:32:30 +0600

    Выполнил работу

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите элементы массива через запятую: ");
+string[] input = Console.ReadLine().Split(',');
+
+string[] output = new string[input.Length];
+int j = 0;
+for (int i = 0; i < input.Length; i++)
+{
+    if (input[i].Length <= 3)
+    {
+        output[j] = input[i];
+        j++;
+    }
+}
+Array.Resize(ref output, j);
+
+Console.WriteLine("Новый массив:");
+for (int i = 0; i < output.Length; i++)
+{
+    Console.WriteLine(output[i]);
+}
